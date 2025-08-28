f71381fb112c9b8f87cca7ee73ba15a5d98a4f76
sanz47 <syemaziz@iut-dhaka.edu>
2025-08-28 12:49:56 +0600

    added server

---
+++ autoflow/client/src/App.jsx
@@ -0,0 +1,6 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import WorkflowAutomationTool from './WorkflowAutomationTool';
+export default function App() { return <WorkflowAutomationTool />; }
